a5d4831c9f4ed7cff28114fcd6403ee48d2a5097
Marc <marc@stlever.com>
2019-01-20 21:33:35 +0700

    add Caret

--- R/main.R
+++ R/main.R
@@ -22,6 +22,9 @@ randomplot <- function(n, dist=c("normal", "uniform")){
   invisible();
 }
 
+usethis::use_package("mlbench")
+usethis::use_package("caret")
+usethis::use_package("lattice")
 usethis::use_package("caTools")
 
 #' Multiple Regression example using Boston housing dataset
@@ -30,34 +33,57 @@ usethis::use_package("caTools")
 #'
 #' @export
 #' @param n numer of random values
-#' @param dist one of "normal" or "uniform".
-regression_model <- function(n, dist){
-  #require(caTools)
+#' @param features one of "normal" or "uniform".
+regression_model <- function(n, features){
+  require(mlbench)
+  require(caret)
+  require(lattice)
+  # require(caTools)
 
-  #input validation
-  #dist <- match.arg(dist, several.ok = T)
-  stopifnot(n < 1e6)
+  data(BostonHousing)
+  df <- BostonHousing
+  # str(df)
+  regVar <- c("age", "lstat", "tax")
+  # str(df[, regVar])
 
-  message <- paste0("dist = ", dist, ". ",
-                    "dist[1] = ", dist[1], ". ",
-                    "dist[2] = ", dist[2], ". ",
-                    "length(dist) = ", length(dist))
+  theme1 <- lattice::trellis.par.get()
+  theme1$plot.symbol$col = rgb(.2, .2, .2, .4)
+  theme1$plot.symbol$pch = 16
+  theme1$plot.line$col = rgb(1, 0, 0, .7)
+  theme1$plot.line$lwd <- 2
+  lattice::trellis.par.set(theme1)
+  caret::featurePlot(x = df[, regVar],
+              y = df$medv,
+              plot = "scatter",
+              type = c("p", "smooth"),
+              span = .5,
+              layout = c(3, 1))
 
-  if ( length(dist) == 1 ) {
-    if(dist == "normal"){
-      graphics::hist(stats::rnorm(n), main=message)
-    }
+  # N_features <- dim(df)[2] - 1 # total number of features
+  # all_features <- colnames(df)
 
-    if(dist == "uniform"){
-      graphics::hist(stats::runif(n), main=message)
-    }
-  } else {
-    if ( (dist[1] == "uniform") && (dist[2] == "normal") ) {
-      graphics::hist(stats::rnorm(n), main="uniform + normal")
-    } else {
-      graphics::hist(stats::rnorm(n), main=paste0("ERROR", message))
-    }
-  }
+  # #input validation
+  # #features <- match.arg(dist, several.ok = T)
+  # stopifnot(length(features) > 0)
+  # stopifnot(length(features) < N_features)
+  # stopifnot(features %in% all_features)
+
+  # message <- paste0("dist=", paste(features, ' '), "/ ",
+  #                   "length=", length(features))
+  #
+  # if ( length(features) == 1 ) {
+  #   if(features == "normal"){
+  #     graphics::hist(stats::rnorm(n), main=message)
+  #   }
+  #
+  #   if(features == "uniform"){
+  #     graphics::hist(stats::runif(n), main=message)
+  #   }
+  # } else {
+  #   if ( (features[1] == "uniform") && (features[2] == "normal") ) {
+  #     graphics::hist(stats::rnorm(n), main=message)
+  #   }
+  # }
 
   #return nothing
   invisible();
