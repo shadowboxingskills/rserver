b74d2a1a4cbdb1422cd7c0a6e4b9208d424d6e39
Marc <marc@stlever.com>
2019-01-20 17:12:45 +0700

    create new regression model

--- R/main.R
+++ R/main.R
@@ -22,4 +22,28 @@ randomplot <- function(n, dist=c("normal", "uniform")){
   invisible();
 }
 
+#' Multiple Regression example using Boston housing dataset
+#'
+#' This function creates a multiple regression model trained with the Boston housing dataset.
+#'
+#' @export
+#' @param n numer of random values
+#' @param dist one of "normal" or "uniform".
+regression_model <- function(n, dist=c("normal", "uniform")){
+  #input validation
+  dist <- match.arg(dist)
+  stopifnot(n < 1e6)
+
+  if(dist == "normal"){
+    hist(rnorm(n), main="SG")
+  }
+
+  if(dist == "uniform"){
+    hist(runif(n))
+  }
+
+  #return nothing
+  invisible();
+}
+
 
