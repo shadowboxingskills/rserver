fbc90212716da47e09845511061cf67d2e53fef6
Marc <sbs@Marcs-MacBook.local>
2019-05-16 19:52:23 +0800

    new dev environment and added leaflet test

--- R/main.R
+++ R/main.R
@@ -333,4 +333,30 @@ regression_model_RMSE <- function(features){
   invisible();
 }
 
+usethis::use_package("leaflet")
+
+#' Example leaflet widget
+#'
+#' Hello world of leaflet htmlwidget wrapped in OpenCPU. Based on example:
+#' \url{http://rstudio.github.io/leaflet/}.
+#'
+#' @export
+#' @param title label for the marker
+#' @param lat lattitude coordinate
+#' @param lng longintude coordinate
+make_map1 <- function(title = "This is a test", lat, lng){
+  m <- leaflet::leaflet()
+  m <- leaflet::addTiles(m)
+  m <- leaflet::addMarkers(m, lng = as.numeric(lng), lat = as.numeric(lat), popup = title)
+  htmlwidgets::saveWidget(m, "mymap1.html", selfcontained = F)
+
+  # df=data.frame(country=c("US", "GB", "BR"),
+  #               val1=c(10,13,14),
+  #               val2=c(23,12,32))
+  # cc <- googleVis::gvisColumnChart(df)
+  # print(cc, 'chart', file='mymap2.html')
+
+}
+
+
 
