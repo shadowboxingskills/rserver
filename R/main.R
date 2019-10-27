9898f19f52336701ca132ab47e6c2da5d002e89a
Marc <marc@shadowboxingskills.com>
2019-10-27 12:05:56 +0700

    added namespace

--- R/main.R
+++ R/main.R
@@ -27,10 +27,10 @@ randomplot <- function(n, dist=c("normal", "uniform", "test")){
     dat <- data.frame(cond = factor(rep(c("A","B"), each=200)), rating = c(rnorm(200),rnorm(200, mean=.8)))
     # Histogram overlaid with kernel density curve
     ggplot2::ggplot(dat, aes(x=rating)) +
-      geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+      ggplot2::geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
                      binwidth=.5,
                      colour="black", fill="white") +
-      geom_density(alpha=.2, fill="#FF6666")  # Overlay with transparent density plot
+      ggplot2::geom_density(alpha=.2, fill="#FF6666")  # Overlay with transparent density plot
   }
 
   #return nothing
